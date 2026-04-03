c2ba18770cf2199663664c58e2507fe3fa08b334
David <meeddry@gmail.com>
2026-04-03 16:15:51 +0300

    fixed_flake8_errors

--- app/main.py
+++ app/main.py
@@ -3,16 +3,26 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+
+def cinema_visit(
+        customers: list, hall_number: int, cleaner: str, movie: str
+) -> None:
     customer_objects = []
 
     for cust_dict in customers:
-        customer_obj = Customer(name=cust_dict["name"], food=cust_dict["food"])
-        CinemaBar.sell_product(product=customer_obj.food, customer=customer_obj)
+        customer_obj = Customer(
+            name=cust_dict["name"],
+            food=cust_dict["food"]
+        )
+        CinemaBar.sell_product(
+            product=customer_obj.food,
+            customer=customer_obj
+        )
         customer_objects.append(customer_obj)
     hall = CinemaHall(hall_number)
     cleaner_obj = Cleaner(cleaner)
-    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_obj)
-
-
-
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaner_obj
+    )
